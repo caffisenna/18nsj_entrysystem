d547a18e670e5e3a7d7d7c47b1c561db1eade18f
caffi <caffi.senna@gmail.com>
2022-01-06 21:54:38 +0900

    add custom validation

--- resources/views/troop_infos/fields.blade.php
+++ resources/views/troop_infos/fields.blade.php
@@ -28,36 +28,54 @@
     <div class="form-group col-sm-6">
         {!! Form::label('person_in_charge_name', '氏名:') !!}
         {!! Form::text('person_in_charge_name', null, ['class' => 'form-control']) !!}
+        @error('person_in_charge_name')
+            <div class="error text-danger">{{ $message }}</div>
+        @enderror
     </div>
 
     <!-- Person In Charge Position Field -->
     <div class="form-group col-sm-6">
         {!! Form::label('person_in_charge_position', '役務:') !!}
         {!! Form::text('person_in_charge_position', null, ['class' => 'form-control']) !!}
+        @error('person_in_charge_position')
+            <div class="error text-danger">{{ $message }}</div>
+        @enderror
     </div>
 
     <!-- Person In Charge Bsid Field -->
     <div class="form-group col-sm-6">
         {!! Form::label('person_in_charge_bsid', '登録番号:') !!}
         {!! Form::text('person_in_charge_bsid', null, ['class' => 'form-control']) !!}
+        @error('person_in_charge_bsid')
+            <div class="error text-danger">{{ $message }}</div>
+        @enderror
     </div>
 
     <!-- Person In Charge Phone Field -->
     <div class="form-group col-sm-6">
         {!! Form::label('person_in_charge_phone', '電話番号:') !!}
         {!! Form::text('person_in_charge_phone', null, ['class' => 'form-control']) !!}
+        @error('person_in_charge_phone')
+            <div class="error text-danger">{{ $message }}</div>
+        @enderror
     </div>
 
     <!-- Person In Charge Cellphone Field -->
     <div class="form-group col-sm-6">
         {!! Form::label('person_in_charge_cellphone', 'ケータイ:') !!}
         {!! Form::text('person_in_charge_cellphone', null, ['class' => 'form-control']) !!}
+        @error('person_in_charge_cellphone')
+            <div class="error text-danger">{{ $message }}</div>
+        @enderror
     </div>
 
     <!-- Person In Charge Email Field -->
     <div class="form-group col-sm-6">
         {!! Form::label('person_in_charge_email', 'Email:') !!}
         {!! Form::text('person_in_charge_email', null, ['class' => 'form-control']) !!}
+        @error('person_in_charge_email')
+            <div class="error text-danger">{{ $message }}</div>
+        @enderror
     </div>
 
 </div>
