7b698360e0fac7a2f578c14694746b0dae3595b5
tta1 <caffi.senna@gmail.com>
2022-01-12 22:23:40 +0900

    Fix#21 (#22)
    * Update edit.blade.php
    
    * update email validation
    
    * show flash message
    
    close #18
    
    * add 班情報
    
    隊基本情報に内包でやってみる
    
    * Update table.blade.php
    
    * メンバー一覧追加
    
    * Update welcome.blade.php
    
    * Update table.blade.php
    
    * Update table.blade.php
    
    * Update edit.blade.php
    
    * Update fields.blade.php
    
    * Update MemberController.php
    
    * Squashed commit of the following:
    
    commit cadd563b3cff2820649283ed6c50f68275778fc1
    Author: caffi <caffi.senna@gmail.com>
    Date:   Wed Jan 12 21:45:15 2022 +0900
    
        update README.md #18
    
        close #18
    
    * fix personal detail #21
    
    close #21

--- resources/views/members/show_fields.blade.php
+++ resources/views/members/show_fields.blade.php
@@ -1,155 +1,115 @@
-<!-- User Id Field -->
-<div class="col-sm-12">
-    {!! Form::label('user_id', 'User Id:') !!}
-    <p>{{ $member->user_id }}</p>
+<div class="uk-card uk-card-default uk-card-body uk-width-1-1@m">
+    <h3 class="uk-card-title">基本情報</h3>
+    <table class="table uk-table-striped uk-table-hover uk-table-small" id="troopInfos-table">
+        <tr>
+            <td>氏名</td>
+            <td>{{ $member->name }} ({{ $member->furigana }})</td>
+        </tr>
+        <tr>
+            <td>性別</td>
+            <td>{{ $member->gender }}</td>
+        </tr>
+        <tr>
+            <td>役務</td>
+            <td>{{ $member->role }}</td>
+        </tr>
+        @if ($member->grade)
+            <tr>
+                <td>進級</td>
+                <td>{{ $member->grade }}</td>
+            </tr>
+        @endif
+        <tr>
+            <td>生年月日</td>
+            <td>{{ $member->birthday }}</td>
+        </tr>
+        @if ($member->patrol_code)
+            <tr>
+                <td>班・役務</td>
+                <td>{{ $member->patrol_code }} {{ $member->patrol_role }}</td>
+            </tr>
+        @endif
+        <tr>
+            <td>登録番号</td>
+            <td>{{ $member->bs_id }}</td>
+        </tr>
+        <tr>
+            <td>研修歴</td>
+            <td>{{ $member->training_record }}</td>
+        </tr>
+        <tr>
+            <td>email</td>
+            <td>{{ $member->email }}</td>
+        </tr>
+        <tr>
+            <td>電話番号</td>
+            <td>{{ $member->phone }}</td>
+        </tr>
+        <tr>
+            <td>ケータイ</td>
+            <td>{{ $member->cell_phone }}</td>
+        </tr>
+    </table>
 </div>
 
-<!-- Role Field -->
-<div class="col-sm-12">
-    {!! Form::label('role', 'Role:') !!}
-    <p>{{ $member->role }}</p>
+<div class="uk-card uk-card-default uk-card-body uk-width-1-1@m">
+    <h3 class="uk-card-title">宗教</h3>
+    <table class="table uk-table-striped uk-table-hover uk-table-small" id="troopInfos-table">
+        <tr>
+            <td>{{ $member->religion }} {{ $member->religion_sect }}</td>
+        </tr>
+    </table>
 </div>
 
-<!-- Patrol Code Field -->
-<div class="col-sm-12">
-    {!! Form::label('patrol_code', 'Patrol Code:') !!}
-    <p>{{ $member->patrol_code }}</p>
+<div class="uk-card uk-card-default uk-card-body uk-width-1-1@m">
+    <h3 class="uk-card-title">原隊</h3>
+    <table class="table uk-table-striped uk-table-hover uk-table-small" id="troopInfos-table">
+        <tr>
+            <td>所属</td>
+            <td>{{ $member->org_dan_name }} {{ $member->org_dan_number }}団 {{ $member->org_group }}隊</td>
+        </tr>
+        <tr>
+            <td>役務</td>
+            <td>{{ $member->org_role }}</td>
+        </tr>
+        @if ($member->org_patrol)
+            <tr>
+                <td>班・役務</td>
+                <td>{{ $member->org_patrol }}班 {{ $member->org_role }}</td>
+            </tr>
+        @endif
+    </table>
 </div>
 
-<!-- Patrol Role Field -->
-<div class="col-sm-12">
-    {!! Form::label('patrol_role', 'Patrol Role:') !!}
-    <p>{{ $member->patrol_role }}</p>
+<div class="uk-card uk-card-default uk-card-body uk-width-1-1@m">
+    <h3 class="uk-card-title">提出物</h3>
+    <table class="table uk-table-striped uk-table-hover uk-table-small" id="troopInfos-table">
+        <tr>
+            <td>SFH</td>
+            <td>{{ $member->sfh }}</td>
+        </tr>
+        <tr>
+            <td>健康調査</td>
+            <td>{{ $member->health_check }}</td>
+        </tr>
+
+    </table>
 </div>
 
-<!-- Bs Id Field -->
-<div class="col-sm-12">
-    {!! Form::label('bs_id', 'Bs Id:') !!}
-    <p>{{ $member->bs_id }}</p>
-</div>
-
-<!-- Name Field -->
-<div class="col-sm-12">
-    {!! Form::label('name', 'Name:') !!}
-    <p>{{ $member->name }}</p>
-</div>
-
-<!-- Furigana Field -->
-<div class="col-sm-12">
-    {!! Form::label('furigana', 'Furigana:') !!}
-    <p>{{ $member->furigana }}</p>
-</div>
-
-<!-- Grade Field -->
-<div class="col-sm-12">
-    {!! Form::label('grade', 'Grade:') !!}
-    <p>{{ $member->grade }}</p>
-</div>
-
-<!-- Gender Field -->
-<div class="col-sm-12">
-    {!! Form::label('gender', 'Gender:') !!}
-    <p>{{ $member->gender }}</p>
-</div>
-
-<!-- Birthday Field -->
-<div class="col-sm-12">
-    {!! Form::label('birthday', 'Birthday:') !!}
-    <p>{{ $member->birthday }}</p>
-</div>
-
-<!-- Religion Field -->
-<div class="col-sm-12">
-    {!! Form::label('religion', 'Religion:') !!}
-    <p>{{ $member->religion }}</p>
-</div>
-
-<!-- Religion Sect Field -->
-<div class="col-sm-12">
-    {!! Form::label('religion_sect', 'Religion Sect:') !!}
-    <p>{{ $member->religion_sect }}</p>
-</div>
-
-<!-- Email Field -->
-<div class="col-sm-12">
-    {!! Form::label('email', 'Email:') !!}
-    <p>{{ $member->email }}</p>
-</div>
-
-<!-- Phone Field -->
-<div class="col-sm-12">
-    {!! Form::label('phone', 'Phone:') !!}
-    <p>{{ $member->phone }}</p>
-</div>
-
-<!-- Cell Phone Field -->
-<div class="col-sm-12">
-    {!! Form::label('cell_phone', 'Cell Phone:') !!}
-    <p>{{ $member->cell_phone }}</p>
-</div>
-
-<!-- Org Dan Name Field -->
-<div class="col-sm-12">
-    {!! Form::label('org_dan_name', 'Org Dan Name:') !!}
-    <p>{{ $member->org_dan_name }}</p>
-</div>
-
-<!-- Org Dan Number Field -->
-<div class="col-sm-12">
-    {!! Form::label('org_dan_number', 'Org Dan Number:') !!}
-    <p>{{ $member->org_dan_number }}</p>
-</div>
-
-<!-- Org Group Field -->
-<div class="col-sm-12">
-    {!! Form::label('org_group', 'Org Group:') !!}
-    <p>{{ $member->org_group }}</p>
-</div>
-
-<!-- Org Patrol Field -->
-<div class="col-sm-12">
-    {!! Form::label('org_patrol', 'Org Patrol:') !!}
-    <p>{{ $member->org_patrol }}</p>
-</div>
-
-<!-- Org Role Field -->
-<div class="col-sm-12">
-    {!! Form::label('org_role', 'Org Role:') !!}
-    <p>{{ $member->org_role }}</p>
-</div>
-
-<!-- Training Record Field -->
-<div class="col-sm-12">
-    {!! Form::label('training_record', 'Training Record:') !!}
-    <p>{{ $member->training_record }}</p>
-</div>
-
-<!-- Uuid Field -->
-<div class="col-sm-12">
-    {!! Form::label('uuid', 'Uuid:') !!}
-    <p>{{ $member->uuid }}</p>
-</div>
-
-<!-- Sfh Field -->
-<div class="col-sm-12">
-    {!! Form::label('sfh', 'Sfh:') !!}
-    <p>{{ $member->sfh }}</p>
-</div>
-
-<!-- Health Check Field -->
-<div class="col-sm-12">
-    {!! Form::label('health_check', 'Health Check:') !!}
-    <p>{{ $member->health_check }}</p>
-</div>
-
-<!-- Created At Field -->
-<div class="col-sm-12">
-    {!! Form::label('created_at', 'Created At:') !!}
-    <p>{{ $member->created_at }}</p>
-</div>
-
-<!-- Updated At Field -->
-<div class="col-sm-12">
-    {!! Form::label('updated_at', 'Updated At:') !!}
-    <p>{{ $member->updated_at }}</p>
+<div class="uk-card uk-card-default uk-card-body uk-width-1-1@m">
+    <h3 class="uk-card-title">データベース</h3>
+    <table class="table uk-table-striped uk-table-hover uk-table-small" id="troopInfos-table">
+        <tr>
+            <td>uuid</td>
+            <td>{{ $member->uuid }}</td>
+        </tr>
+        <tr>
+            <td>created_at</td>
+            <td>{{ $member->created_at }}</td>
+        </tr>
+        <tr>
+            <td>updated_at</td>
+            <td>{{ $member->updated_at }}</td>
+        </tr>
+    </table>
 </div>
