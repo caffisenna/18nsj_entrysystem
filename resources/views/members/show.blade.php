6006a2681200e97778ebe9546ab08966adffc6a1
caffi <caffi.senna@gmail.com>
2022-01-12 22:22:30 +0900

    fix personal detail #21
    close #21

--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -5,11 +5,11 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Member Details</h1>
+                    <h1>詳細</h1>
                 </div>
                 <div class="col-sm-6">
                     <a class="btn btn-default float-right" href="{{ route('members.index') }}">
-                        Back
+                        戻る
                     </a>
                 </div>
             </div>
