7f20e61721ec841323aeef3e63fa8346ce504a47
caffi <caffi.senna@gmail.com>
2022-01-14 21:06:03 +0900

    update show.blade.php #30
    close #30

--- resources/views/members/show.blade.php
+++ resources/views/members/show.blade.php
@@ -8,15 +8,21 @@
                     <h1>詳細</h1>
                 </div>
                 <div class="col-sm-6">
-                    <a class="btn btn-default float-right" href="{{ route('members.index') }}">
-                        戻る
-                    </a>
+
                 </div>
             </div>
         </div>
     </section>
 
     <div class="content px-3">
+        <div class='btn-group'>
+            <a href="{{ route('members.edit', [$member->id]) }}" class='btn btn-warning float-right'>
+                <i class="far fa-edit"></i>編集
+            </a>
+            <a class="btn btn-default float-right" href="{{ route('members.index') }}">
+                戻る
+            </a>
+        </div>
         <div class="card">
 
             <div class="card-body">
