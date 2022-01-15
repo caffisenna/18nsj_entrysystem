2013ee794a3f54722891287ca4a6a0d1c989e5c5
caffi <caffi.senna@gmail.com>
2022-01-15 11:49:47 +0900

    fix #39
    close #39

--- resources/views/members/create.blade.php
+++ resources/views/members/create.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-12">
-                    <h1>Create Member</h1>
+                    <h1>メンバー登録</h1>
                 </div>
             </div>
         </div>
@@ -28,8 +28,8 @@
             </div>
 
             <div class="card-footer">
-                {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-                <a href="{{ route('members.index') }}" class="btn btn-default">Cancel</a>
+                {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!}
+                <a href="{{ route('members.index') }}" class="btn btn-default">キャンセル</a>
             </div>
 
             {!! Form::close() !!}
