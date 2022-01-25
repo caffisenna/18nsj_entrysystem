4a60f8bf3ffa9bf2fa6c6a7598287c10a65cc4f3
caffi <caffi.senna@gmail.com>
2022-01-25 20:58:13 +0900

    Update show.blade.php

--- resources/views/admin/troop_members/show.blade.php
+++ resources/views/admin/troop_members/show.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>詳細1</h1>
+                    <h1>詳細admin</h1>
                 </div>
                 <div class="col-sm-6">
 
