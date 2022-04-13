7c025961087d98338862720032caf44a78a40b5c
caffi <caffi.senna@gmail.com>
2022-04-13 16:26:25 +0900

    Update show_fields.blade.php

--- resources/views/admin/volstaffs/show_fields.blade.php
+++ resources/views/admin/volstaffs/show_fields.blade.php
@@ -1,146 +1,112 @@
 <div class="uk-card uk-card-default uk-card-body uk-width-1-1@m">
-    <h3 class="uk-card-title">基本情報</h3>
+    <h3 class="uk-card-title">スタッフ登録情報</h3>
+    <table class="uk-table uk-table-striped">
+        <tr>
+            <th>氏名</th>
+            <td>{{ Auth::user()->name }} ({{ $volstaff->furigana }})</td>
+        </tr>
+        <tr>
+            <th>所属団</th>
+            <td>{{ $volstaff->org_district }}地区 {{ $volstaff->org_dan_name }}{{ $volstaff->org_dan_number }}団 {{ $volstaff->org_group }}@unless ($volstaff->org_group == "団")隊@endunless</td>
+        </tr>
+        <tr>
+            <th>地区役務</th>
+            <td>{{ $volstaff->district_role }}</td>
+        </tr>
+        <tr>
+            <th>ベース名</th>
+            <td>{{ $volstaff->camp_area }}</td>
+        </tr>
+        <tr>
+            <th>奉仕部署</th>
+            <td>{{ $volstaff->job_department }}</td>
+        </tr>
+    </table>
 
-    <!-- Name Field -->
-    <div class="col-sm-12">
-        {!! Form::label('name', '氏名:') !!}
-        {{ $volstaff->user->name }} ({{ $volstaff->furigana }})
-    </div>
-
-    <!-- Camp Area Field -->
-    <div class="col-sm-12">
-        {!! Form::label('camp_area', 'キャンプ場:') !!}
-        {{ $volstaff->camp_area }}
-    </div>
-
-    <!-- Job Department Field -->
-    <div class="col-sm-12">
-        {!! Form::label('job_department', '奉仕部署:') !!}
-        {{ $volstaff->job_department }}
-    </div>
+    @unless($volstaff->job_department)
+        <div class="uk-card uk-card-default uk-card-hover uk-card-body">
+            <h3 class="uk-card-title">希望部署(未定の場合)</h3>
+            <table class="uk-table uk-table-striped">
+                <tr>
+                    <th>第1希望</th>
+                    <td>{{ $volstaff->choice1_camp_area }} {{ $volstaff->choice1_job_department }}</td>
+                </tr>
+                <tr>
+                    <th>第2希望</th>
+                    <td>{{ $volstaff->choice2_camp_area }} {{ $volstaff->choice2_job_department }}</td>
+                </tr>
+                <tr>
+                    <th>第3希望</th>
+                    <td>{{ $volstaff->choice3_camp_area }} {{ $volstaff->choice3_job_department }}</td>
+                </tr>
+            </table>
+        </div>
+    @endunless
 
     <!-- How To Join Field -->
-    <div class="col-sm-12">
-        {!! Form::label('how_to_join', '参加日:') !!}
-        @if ($volstaff->how_to_join != '全期間')
-            @if ($volstaff->join_days)
-                合計: {{ substr_count($volstaff->join_days, '月') }}日間 / 内訳: {{ $volstaff->join_days }}
-            @endif
-        @else
-            {{ $volstaff->how_to_join }}
-        @endif
-    </div>
-
-    <!-- Bs Id Field -->
-    <div class="col-sm-12">
-        {!! Form::label('bs_id', '登録番号:') !!}
-        {{ $volstaff->bs_id }}
-    </div>
-
-    <!-- Gender Field -->
-    <div class="col-sm-12">
-        {!! Form::label('gender', '性別:') !!}
-        {{ $volstaff->gender }}
-    </div>
-
-    <!-- Birthday Field -->
-    <div class="col-sm-12">
-        {!! Form::label('birthday', '生年月日:') !!}
-        {{ $volstaff->birthday->format('Y-m-d') }}
-    </div>
-
-    <!-- Email Field -->
-    <div class="col-sm-12">
-        {!! Form::label('email', 'Email:') !!}
-        {{ $volstaff->user->email }}
-    </div>
-
-    <!-- Phone Field -->
-    <div class="col-sm-12">
-        {!! Form::label('phone', '自宅電話:') !!}
-        {{ $volstaff->phone }}
-    </div>
-
-    <!-- Cell Phone Field -->
-    <div class="col-sm-12">
-        {!! Form::label('cell_phone', 'ケータイ:') !!}
-        {{ $volstaff->cell_phone }}
-    </div>
-
-    <!-- Training Record Field -->
-    <div class="col-sm-12">
-        {!! Form::label('training_record', '研修歴:') !!}
-        {{ $volstaff->training_record }}
-    </div>
+    <table class="uk-table uk-table-striped">
+        <tr>
+            <th>参加日</th>
+            <td>
+                @if ($volstaff->how_to_join != '全期間')
+                    合計: {{ substr_count($volstaff->join_days, ',') + 1 }}日間 / 内訳: {{ $volstaff->join_days }}
+                @else
+                    {{ $volstaff->how_to_join }}
+                @endif
+            </td>
+        </tr>
+        <tr>
+            <th>大集会参加希望</th>
+            <td>{{ $volstaff->event_0807 }}</td>
+        </tr>
+        <tr>
+            <th>登録番号</th>
+            <td>{{ $volstaff->bs_id }}</td>
+        </tr>
+        <tr>
+            <th>性別</th>
+            <td>{{ $volstaff->gender }}</td>
+        </tr>
+        <tr>
+            <th>生年月日</th>
+            <td>{{ $volstaff->birthday->format('Y-m-d') }}</td>
+        </tr>
+        <tr>
+            <th>Email</th>
+            <td>{{ Auth::user()->email }}</td>
+        </tr>
+        <tr>
+            <th>自宅電話</th>
+            <td>{{ $volstaff->phone }}</td>
+        </tr>
+        <tr>
+            <th>ケータイ</th>
+            <td>{{ $volstaff->cell_phone }}</td>
+        </tr>
+        <tr>
+            <th>研修歴</th>
+            <td>{{ $volstaff->training_record }}</td>
+        </tr>
+        <tr>
+            <th>車両情報</th>
+            <td>{{ $volstaff->car_number }}<br>{{ $volstaff->car_type }}</td>
+        </tr>
+        {{-- <tr>
+            <th>SFH</th>
+            <td>{{ $volstaff->sfh }}</td>
+        </tr> --}}
+        {{-- <tr>
+            <th>健康調査</th>
+            <td>{{ $volstaff->health_check }}</td>
+        </tr> --}}
+        <tr>
+            <th>特記事項</th>
+            <td>{{ $volstaff->memo }}</td>
+        </tr>
+    </table>
 </div>
 
-<div class="uk-card uk-card-default uk-card-body uk-width-1-1@m">
-    <h3 class="uk-card-title">所属団</h3>
-    <!-- Org Dan Name Field -->
-    <div class="col-sm-12">
-        {!! Form::label('org_dan_name', '所属団:') !!}
-        {{ $volstaff->org_district }}地区 {{ $volstaff->org_dan_name }}{{ $volstaff->org_dan_number }}団
-        {{ $volstaff->org_group }}隊
-    </div>
-
-    <!-- Org Role Field -->
-    <div class="col-sm-12">
-        {!! Form::label('org_role', '役務:') !!}
-        {{ $volstaff->org_role }}
-    </div>
-</div>
-
-<div class="uk-card uk-card-default uk-card-body uk-width-1-1@m">
-    <h3 class="uk-card-title">所属地区</h3>
-    <!-- District Role Field -->
-    <div class="col-sm-12">
-        {!! Form::label('district_role', '役務:') !!}
-        {{ $volstaff->org_district }}地区 {{ $volstaff->district_role }}
-    </div>
-</div>
-
-<div class="uk-card uk-card-default uk-card-body uk-width-1-1@m">
-    <h3 class="uk-card-title">車両情報</h3>
-    <!-- Car Number Field -->
-    <div class="col-sm-12">
-        {!! Form::label('car_number', 'ナンバー:') !!}
-        {{ $volstaff->car_number }}
-    </div>
-
-    <!-- Car Type Field -->
-    <div class="col-sm-12">
-        {!! Form::label('car_type', '車種:') !!}
-        {{ $volstaff->car_type }}
-    </div>
-</div>
-
-<div class="uk-card uk-card-default uk-card-body uk-width-1-1@m">
-    <h3 class="uk-card-title">その他</h3>
-    <!-- Sfh Field -->
-    <div class="col-sm-12">
-        {!! Form::label('sfh', 'SFH:') !!}
-        {{ $volstaff->sfh }}
-    </div>
-
-    <!-- Health Check Field -->
-    <div class="col-sm-12">
-        {!! Form::label('health_check', '健康調査:') !!}
-        {{ $volstaff->health_check }}
-    </div>
-
-    <!-- 大集会参加希望 Field -->
-    <div class="col-sm-12">
-        {!! Form::label('event_0807', '大集会参加希望:') !!}
-        {{ $volstaff->event_0807 }}
-    </div>
-
-    <!-- Memo Field -->
-    <div class="col-sm-12">
-        {!! Form::label('memo', '特記事項:') !!}
-        {{ $volstaff->memo }}
-    </div>
-</div>
-
+<p class="uk-text-danger uk-text-large">参加費振込は別途ご案内しますのでお待ちください。</p>
 <div class="uk-card uk-card-primary uk-card-body uk-width-1-1@m">
     <h3 class="uk-card-title">参加費見積</h3>
     <ul>
@@ -154,8 +120,8 @@
             @if ($volstaff->how_to_join == '全期間')
                 <td>27,000円</td>
             @else
-                <td>{{ number_format(4000 * (substr_count($volstaff->join_days, '月'))) }}円
-                    (4,000円 x {{ substr_count($volstaff->join_days, '月')}}日間)</td>
+                <td>{{ number_format(4000 * (substr_count($volstaff->join_days, ',') + 1)) }}円
+                    (4,000円 x {{ substr_count($volstaff->join_days, ',') + 1 }}日間)</td>
             @endif
         </tr>
         <tr>
@@ -180,6 +146,15 @@
             <td>
                 <h4>{{ number_format($volstaff->total_fee) }} 円</h4>
             </td>
+        </tr>
+        <tr>
+            <td>振込口座</td>
+            <td>GMOあおぞらネット銀行 法人営業部 普通 1331429<br>
+                シヤ）ニホンボーイスカウトトウキヨウレンメイ</td>
+        </tr>
+        <tr>
+            <td>事務局確認</td>
+            <td></td>
         </tr>
     </table>
 
