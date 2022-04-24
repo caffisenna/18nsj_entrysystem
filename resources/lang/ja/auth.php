a8f28f0fe4c1c2298435b3ad272f9858c445720b
caffi <caffi.senna@gmail.com>
2022-04-24 11:38:54 +0900

    feature #126
    close #126

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'ユーザー名またはパスワードが照合されませんでした。',
+    'password' => 'パスワードが違います',
+    'throttle' => '多数のログインに失敗しました。 :seconds 秒後に試してください。',
+
+];
---
+++ resources/lang/ja/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'パスワードがリセットされました!',
+    'sent' => 'パスワードのリセットリンクを送信しました。',
+    'throttled' => 'リトライの前に少しお待ちください。',
+    'token' => 'パスワードのリセットトークンは無効です。',
+    'user' => "そのメールアドレスでユーザー登録が確認できませんでした。",
+];
